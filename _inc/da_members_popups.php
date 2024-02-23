ade6ad68272b088788fd82a9a9f1a8e58d7e4bca
irfan-deva <irfandeva@gmail.com>
2024-02-23 14:17:40 +0530

    created a confirm dialog
    confirm dialog will show when admin will hit the delete button

---
+++ _inc/da_members_popups.php
@@ -0,0 +1,14 @@
+<?php
+function del_popup() {
+?>
+  <div class="popup_container">
+    <div class="del_popup">
+      <span>Are you sure you want to delete this member</span>
+      <div class="del_popup_actions">
+        <button id="no">Cancel</button>
+        <button id="yes">Yes</button>
+      </div>
+    </div>
+  </div>
+<?php
+}
