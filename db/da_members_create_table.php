9f507bedfadfecb31063ee9d217f64bf3d5184a4
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-02-26 17:19:20 +0530

    add initial form inputs to db

--- db/da_members_create_table.php
+++ db/da_members_create_table.php
@@ -11,8 +11,15 @@ function create_table() {
      bio varchar(50) NOT NULL,
      country varchar(20) NOT NULL,
      designation varchar(50) NOT NULL,
+     address varchar(50) NOT NULL,
+     consituency varchar(20) NOT NULL,
+     member_type varchar(50) NOT NULL,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
      PRIMARY KEY (id)
    ) $charset_collate;";
+
 
   $table_name_forum_inputs = $wpdb->prefix . 'form_inputs';
   $daMembersFormInputsTableQuery = "DROP TABLE IF EXISTS $table_name_forum_inputs;
@@ -30,4 +37,35 @@ function create_table() {
 
   dbDelta($daMembersTableQuery);
   dbDelta($daMembersFormInputsTableQuery);
+
+  //  initial form inputs
+  // (type,label,data_type,size,requird)
+  $inputs = array(
+    array('text', 'first_name', 'varchar', '20', '1'),
+    array('text', 'last_name', 'varchar', '20', '1'),
+    array('text', 'bio', 'varchar', '50', '0'),
+    array('select', 'country', 'varchar', '20', '0'),
+    array('text', 'designation', 'varchar', '20', '0'),
+    array('text', 'address', 'varchar', '20', '0'),
+    array('text', 'consituency', 'varchar', '20', '0'),
+    array('text', 'member_type', 'varchar', '20', '0'),
+  );
+
+  // Prepare data for insertion
+  $data_to_insert = array();
+  foreach ($inputs as $input) {
+    var_dump($input);
+    $data_to_insert[] = array(
+      'type' => $input[0],
+      'label' => $input[1],
+      'data_type' => $input[2],
+      'size' => $input[3],
+      'required' => $input[4],
+
+    );
+  }
+
+  foreach ($data_to_insert as $dti) {
+    $wpdb->insert($table_name_forum_inputs, $dti);
+  }
 }
