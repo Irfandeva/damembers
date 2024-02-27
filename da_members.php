b94caf6bda60698a16882cf747448876653a72df
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-02-27 11:22:33 +0530

    changed labels in manage form page

--- da_members.php
+++ da_members.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: DA Members
  * Plugin URI: localhost
- * Description: A plugin for DA Members
+ * Description: A plugin for DA Members.
  * Version: 1.0
  * Author: Irfan Farooq Deva
  * Author URI: not found
@@ -11,7 +11,6 @@
 
 
 require(plugin_dir_path(__FILE__) . '/db/da_members_create_table.php');
-
 register_activation_hook(__FILE__, 'create_table');
 
 function enqueue_css_js($hook) {
@@ -23,12 +22,11 @@ function enqueue_css_js($hook) {
 }
 add_action('admin_enqueue_scripts', 'enqueue_css_js');
 
-
 function admin_menu_item() {
   add_menu_page('DA Members', 'DA Members', 'manage_options', 'da-members', 'da_members_show', 'dashicons-schedule', 3);
   add_submenu_page('', 'Add Member', 'Add Member', 'manage_options', 'da-members-add', 'da_members_add');
   add_submenu_page('', 'Edit Member', 'Edit Member', 'manage_options', 'da-members-edit', 'da_members_edit');
-  add_submenu_page('da-members', 'Edit Form', 'Edit Form', 'manage_options', 'edit-form', 'edit_form');
+  add_submenu_page('da-members', 'Manage Form Fields', 'Manage Form Fields', 'manage_options', 'edit-form', 'edit_form');
 }
 add_action('admin_menu', 'admin_menu_item');
 add_action('admin_footer', 'del_popup');
