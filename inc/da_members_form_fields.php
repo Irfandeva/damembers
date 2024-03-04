2e6bc7cdee9fc184fc2f24caab4f9bd7cdbd79b4
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-04 11:16:10 +0530

    removed one by one form fields update, implemented bulk update instead

--- inc/da_members_form_fields.php
+++ inc/da_members_form_fields.php
@@ -1,84 +1,62 @@
 <?php
 function manageFormFields() {
+
   global $wpdb;
 
   $da_members_form_fields_table = $wpdb->prefix . 'da_members_form_fields';
-  $table_name_members = $wpdb->prefix . 'da_members';
-  $form_fields = $wpdb->get_results("SELECT * FROM $da_members_form_fields_table");
+  $da_members_members = $wpdb->prefix . 'da_members';
 
-  //EDIT/UPDATE EXIXTING FORM INPUT/FIELD
-  if (isset($_POST['form_input_submit_upt'])) {
-    $id = sanitize_text_field($_POST['id']);
-    $label = sanitize_text_field($_POST['label']);
-    $priority = sanitize_text_field($_POST['priority']);
-    echo "$id, $label, $priority";
-    $updateQuery = $wpdb->query("UPDATE $da_members_form_fields_table SET  label='$label', priority='$priority' WHERE id='$id'");
-
-    if (!$updateQuery) {
-      echo "ERROR UPDATING FORM FIELD" . $wpdb->last_error;
-    } else {
-      echo "UPDATED SUCCESSFULLY";
-      //if new from input was added to db successfully, we will create a new table column for that input in table to store values later.
-      // $dataType = $data_type . "(" . $size . ")";
-      // $wpdb->query($wpdb->prepare("ALTER TABLE $table_name_members ADD $label $dataType"));
-    }
+  //ADD NEW FORM FIELD
+  if (isset($_POST['submit_new_form_field'])) {
+    $field_label = add_($_POST['label']);
+    $field_priority = $_POST['priority'];
+    $wpdb->query($wpdb->prepare("INSERT INTO $da_members_form_fields_table (label, priority,field_type) 
+                                 VALUES (%s, %s,%s)", $field_label, $field_priority, 'text'));
+    $wpdb->query("ALTER TABLE $da_members_members ADD $field_label varchar(50);");
   }
 
+  //EDIT/UPDATE EXIXTING FORM FIELD
+  if (isset($_POST['form_input_submit_upt'])) {
+    if (isset($_POST['ids'])) {
+      $labels = ($_POST['labels']);
+      $priorities = $_POST['priorities'];
+      $ids = $_POST['ids'];
+
+      foreach ($ids as $id) {
+        $u_label = add_($labels[$id]);
+        $u_priority = $priorities[$id];
+        $updateQuery = $wpdb->query("UPDATE $da_members_form_fields_table SET  label='$u_label', priority='$u_priority' WHERE id='$id'");
+      }
+    } else {
+      echo "NO FIELD SELECTED.";
+    }
+  }
+  echo $wpdb->last_error
 ?>
   <div class="wrap">
-    <h2 class="wp-heading-inline">Manage Form Fields</h2>
-    <table class="wp-list-table widefat striped">
-      <thead>
-        <tr>
-          <th width="33%">Label</th>
-          <th width="33%">Priority</th>
-          <th width="33%">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        foreach ($form_fields as $form_field) {
-        ?>
-          <tr>
-            <form action="" method="post">
-              <td><input type="text" id="label" name="label" value='<?php echo $form_field->label; ?>'></td>
-              <td><input type="number" id="priority" name="priority" value='<?php echo $form_field->priority; ?>'></td>
-              <td>
-                <input type="hidden" id="id" name="id" value='<?php echo $form_field->id; ?>'>
-                <!-- <button id="form_input_submit_upt" name="form_input_submit_del" type="submit">UPDATE</button> -->
-                <input type="submit" name="form_input_submit_upt" id="form_input_submit_upt" class="button button-primary" value="Update">
-                <input type="submit" name="form_input_submit_del" id="form_input_submit_del" class="button button-primary" value="Delete">
-                <!-- <button id="form_input_submit_del" name="form_input_submit_del" type="submit">DELETE</button> -->
-              </td>
-            </form>
-          </tr>
+    <a href="http://localhost/wordpress/wp-admin/admin.php?page=da-members" style="text-decoration: none" class="page-title-action">&larr; GO BACK</a>
+    <!-- <h1 class="wp-heading-inline">Manage Form Fields</h1> -->
+    <?php
+    $form_fields = $wpdb->get_results("SELECT * FROM $da_members_form_fields_table");
+    echo '<form method="post" action="" >';
+    echo '<h5 class="wp-heading-inline">Edit</h5>';
+    foreach ($form_fields as $form_field) {
+      echo "<div style='display:flex;gap:16px;align-items:center;margin-bottom:10px'>";
+      echo '<input type="checkbox" name="ids[]" value="' . $form_field->id . '">';
+      echo '<input type="text" name="labels[' . $form_field->id . ']" value="' . remove_($form_field->label) . '">';
+      echo '<input type="number" name="priorities[' . $form_field->id . ']" value="' . $form_field->priority . '">';
+      echo "</div>";
+    }
+    echo '<input type="submit" name="form_input_submit_upt" class="button button-primary" value="Update">';
+    echo '</form>';
+    ?>
 
-        <?php
-        } ?>
-      </tbody>
-    </table>
-
-    <!-- add new form input -->
-
-    <table class="wp-list-table widefat striped" style="margin-top: 20px;">
-      <thead>
-        <tr>
-          <th width="33%">Label</th>
-          <th width="33%">Priority</th>
-          <th width="33%">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <form action="" method="post">
-            <td><input type="text" id="label" name="label"></td>
-            <td><input type="number" id="priority" name="priority"></td>
-            <td>
-              <input type="submit" id="form_inew_from_input_submit" name="form_inew_from_input_submit" class="button button-primary" value="Save New Form Field">
-            </td>
-          </form>
-        </tr>
-      </tbody>
-    </table>
-  <?php
+    <form action="" method="post">
+      <h5 class="wp-heading-inline">Add new</h5>
+      <input type="text" id="label" name="label">
+      <input type="number" id="priority" name="priority">
+      <input type="submit" id="submit_new_form_field" name="submit_new_form_field" class="button button-primary" value="Save New Form Field">
+    </form>
+  </div>
+<?php
 }
