4c6b29b1cc6dd3157331d5465cc2b07f9e9da6ee
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-08 09:33:39 +0530

    added utility function for checking simillar pattern in two strings

--- utils/helper_fns.php
+++ utils/helper_fns.php
@@ -4,6 +4,11 @@ function log_it($val) {
   var_dump($val);
   echo "</pre>";
 }
+function echo_it($val) {
+  echo "<pre>";
+  echo ($val);
+  echo "</pre>";
+}
 //replace whitespace with _ and make every word lower case.
 function add_($str) {
   if (!empty($str))
@@ -13,4 +18,22 @@ function add_($str) {
 function remove_($str) {
   if (!empty($str))
     return ucwords(str_replace('_', ' ', $str));
+}
+function trim_and_tolowercase($str) {
+  if (!empty($str))
+    return trim(strtolower($str));
+}
+//find match between two strings
+function find_match($pattern, $string) {
+  echo $pattern . $string . "<br>";
+  $pattern = trim_and_tolowercase($pattern);
+  $string = trim_and_tolowercase($string);
+  // $pattern = str_replace(' ', '', $pattern);
+  // $string = str_replace(' ', '', $string);
+
+  if ($pattern == $string || preg_match("/$pattern/", $string) || preg_match("/$string/", $pattern)) {
+    return true;
+  } else {
+    return false;
+  }
 }
