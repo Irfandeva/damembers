fa82ce3cb9f1017bcfddc791aac1cba0c5db7ed7
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-11 15:52:23 +0530

    added ability to controle number of table columns and records per page

--- inc/da_members_settings.php
+++ inc/da_members_settings.php
@@ -2,9 +2,12 @@
 function da_members_settings() {
   if (isset($_POST['save_per_page_value'])) {
     $new_records_per_page = !empty($_POST['r-p-p']) ? $_POST['r-p-p'] : 20;
+    $new_number_of_poperties_columns = !empty($_POST['t-c']) ? $_POST['t-c'] : 5;
     update_option('records_per_page', $new_records_per_page);
+    update_option('number_of_poperties_columns', $new_number_of_poperties_columns);
   }
   $records_per_page = get_option('records_per_page', 20);
+  $number_of_poperties_columns = get_option('number_of_poperties_columns', 5);
 ?>
   <div class="wrap">
     <h1>DA Members Settings</h1>
@@ -12,6 +15,10 @@ function da_members_settings() {
       <div class="input-item" style="flex-direction: row;gap:30px">
         <label for="r-p-p">Records Per Page</label>
         <input type="number" name="r-p-p" id="r-p-p" style="width:70px" value='<?php echo $records_per_page; ?>'>
+      </div>
+      <div class="input-item" style="flex-direction: row;gap:30px">
+        <label for="r-p-p">Table Columns</label>
+        <input type="number" name="t-c" id="t-c" style="width:70px" value='<?php echo $number_of_poperties_columns; ?>'>
       </div>
       <input type="submit" name="save_per_page_value" value="Save Changes" class="button button-primary" style="margin-top:20px;">
     </form>
