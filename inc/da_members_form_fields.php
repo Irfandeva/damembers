f88e9eff0d490aa3939b91cc7fa9c1cf8d57dcf3
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-11 15:48:38 +0530

    replace dropdown with checkbox for required column,
    stop user from submitting if user chooses same priority for multiple labels'
    
    show proper error messages

--- inc/da_members_form_fields.php
+++ inc/da_members_form_fields.php
@@ -4,30 +4,46 @@ function manageFormFields() {
   global $wpdb;
   $da_members_form_fields_table = $wpdb->prefix . 'da_members_form_fields';
   $total_form_fields = count($wpdb->get_results("SELECT * FROM $da_members_form_fields_table"));
-
+  $result = array();
+  $result['status'] = 'ok';
+  $result['message'] = '';
   //EDIT/UPDATE EXIXTING FORM FIELD
+  $res_check = array();
   if (isset($_POST['form_input_submit_upt'])) {
-    if (isset($_POST['ids'])) {
+    $priorities = $_POST['priorities'];
+    $hasDuplicates = count($priorities) > count(array_unique($priorities));
+    if ($hasDuplicates) {
+      // echo "<div id='message' class='notice error'><p>Duplicate priorities.</p></div>";
+      $result['status'] = 'error';
+      $result['message'] = 'Duplicate priorities, two labels can not have same priority';
+    }
+    if (!isset($_POST['ids'])) {
+      $result['status'] = 'error';
+      $result['message'] = 'No Fields Selected.';
+    }
+    if ($result['status'] !== 'error') {
       $labels = ($_POST['labels']);
-      $priorities = $_POST['priorities'];
-      $required_ids = $_POST['required_ids'];
       $ids = $_POST['ids'];
       foreach ($ids as $id) {
         $u_label = $labels[$id];
-        $u_required = $required_ids[$id];
+        $is_required = "required_ids_" . $id;
+        $u_required = isset($_POST[$is_required]) ? '1' : 0;
         $u_priority = $priorities[$id];
         $updateQuery = $wpdb->query("UPDATE $da_members_form_fields_table SET  label='$u_label', priority='$u_priority',required='$u_required' WHERE id='$id'");
-        if ($updateQuery) {
-          echo "<div id='message' class='notice is-dismissible updated'>
-              <p>Field Updated successfully</p><button type='button' class='notice-dismiss'>
-              <span class='screen-reader-text'>Dismiss this notice.</span></button></div>";
-        } else {
-          echo " <div id='message' class='notice error'><p>error updating fields.</p></div>";
-        }
+        if ($updateQuery)
+          array_push($res_check, $updateQuery);
       }
-    } else {
-      echo " <div id='message' class='notice error'><p>No Fields Selected.</p></div>";
     }
+  }
+
+  if (!empty($res_check)) {
+    $msg = count($res_check) > 1 ? 'Fields' : 'Field';
+    $result['status'] = 'ok';
+    $result['message'] = $msg . " Updated successfully";
+
+    // echo "<div id='message' class='notice is-dismissible updated'>
+    //           <p>" . $msg . " Updated successfully</p><button type='button' class='notice-dismiss'>
+    //           <span class='screen-reader-text'>Dismiss this notice.</span></button></div>";
   }
   echo $wpdb->last_error
 ?>
@@ -35,6 +51,20 @@ function manageFormFields() {
     <a href="http://localhost/wordpress/wp-admin/admin.php?page=da-members" style="text-decoration: none" class="page-title-action">&larr; GO BACK</a>
     <h1 class="wp-heading-block">Manage Form Fields</h1>
     <?php
+
+
+    if (isset($result) && $result['message'] !== '') {
+      if ($result['status'] == 'ok') {
+        echo "<div id='message' class='notice is-dismissible updated'>
+    <p>" . $result['message'] . "</p><button type='button' class='notice-dismiss'>
+    <span class='screen-reader-text'>Dismiss this notice.</span></button></div>";
+      } elseif ($result['status'] == 'error') {
+        echo "<div id='message' class='notice error'><p>" . $result['message'] . "</p></div>";
+      }
+    }
+
+
+
     $form_fields = $wpdb->get_results("SELECT * FROM $da_members_form_fields_table");
     ?>
     <form method="post" action="">
@@ -56,13 +86,10 @@ function manageFormFields() {
               '<input type="checkbox" name="ids[]" value="' . $form_field->id . '" id="' . $form_field->id . '">' .
               '<input type="text" name="field" value="' . $form_field->field_name . '" readonly style="color:#999">' .
               "</td>";
-            echo "<td>" . '<input type="text" name="labels[' . $form_field->id . ']" value="' . $form_field->label . '" data-id="' . $form_field->id . '" class="form-field-input">' . "</td>";
 
-            echo "<td>" . '<select name="required_ids[' . $form_field->id . ']" id=$form_field->field_name>';
-            $selected_yes = ($form_field->required == '1') ? 'selected' : '';
-            $selected_no = ($form_field->required == '0') ? 'selected' : '';
-            echo "<option value='1' $selected_yes>yes</option>";
-            echo "<option value='0' $selected_no>no</option>";
+            echo "<td>" . '<input type="text" name="labels[' . $form_field->id . ']" value="' . $form_field->label . '" data-id="' . $form_field->id . '" class="form-field-input">' . "</td>";
+            $checked = ($form_field->required === '1') ? 'checked' : '';
+            echo "<td>" . "<input type='checkbox' name='required_ids_" . $form_field->id . "'$checked class='req'>";
             echo "</select>" . "</td>";
 
             echo "<td>" . '<select name="priorities[' . $form_field->id . ']" id=$form_field->field_name>';
