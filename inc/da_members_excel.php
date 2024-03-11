dbaf13f11db8a2e8f838654c70030c66525b0954
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-11 15:57:50 +0530

    show error message if user skips/ignores required fields for mapping,

--- inc/da_members_excel.php
+++ inc/da_members_excel.php
@@ -12,56 +12,60 @@ function daMembersDownload() {
   foreach ($cols as $col) {
     $member_columns_names[] = $col->Field;
   }
-  $excelHeader = "";
+  $excel_header = "";
   foreach ($member_columns_names as $label) {
-    $formattedLabel = ucwords(str_replace('_', ' ', $label));
-    $excelHeader .= $formattedLabel . "\t";
+    $formatted_label = ucwords(str_replace('_', ' ', $label));
+    $excel_header .= $formatted_label . "\t";
   }
   // Find the position of the last occurrence of '\t'
-  $lastTabIndex = strrpos($excelHeader, "\t");
+  $last_tab_index = strrpos($excel_header, "\t");
   // Replace the last occurrence of '\t' with '\n'
-  $excelContent = substr_replace($excelHeader, "\n", $lastTabIndex, 1);
+  $excel_content = substr_replace($excel_header, "\n", $last_tab_index, 1);
   if (isset($_GET['download'])) {
     $results = $wpdb->get_results("SELECT * FROM $da_members_table");
     foreach ($results as $row) {
       foreach ($member_columns_names as $label) {
-        $excelContent .= $row->$label . "\t";
+        $excel_content .= $row->$label . "\t";
       }
       // Find the position of the last occurrence of '\t'
-      $lastTabIndex = strrpos($excelContent, "\t");
+      $last_tab_index = strrpos($excel_content, "\t");
       // Replace the last occurrence of '\t' with '\n'
-      $excelContent = substr_replace($excelContent, "\n", $lastTabIndex, 1);
+      $excel_content = substr_replace($excel_content, "\n", $last_tab_index, 1);
     }
-    $fileName = "da_members_" . date("Y-m-d") . ".xls";
+    $file_name = "da_members_" . date("Y-m-d") . ".xls";
     // Set headers for Excel download
     header("Content-Type: application/vnd.ms-excel");
-    header("Content-Disposition: attachment; filename=\"$fileName\"");
+    header("Content-Disposition: attachment; filename=\"$file_name\"");
     // Output Excel content
-    echo $excelContent;
+    echo $excel_content;
     // Exit to prevent any additional content from being output
     exit;
   }
 }
 function uploadFromExcel() {
-  $totalCount = 0;
-  $emptyRecords = 0;
+  $total_count = 0;
+  $empty_records = 0;
   $result = array();
+  $result['status'] = '';
+  $result['message'] = '';
 
   global $wpdb;
+
   require_once(plugin_dir_path(__DIR__) . 'vendor/autoload.php');
   $da_members_table = DA_MEMBERS_TABLE;
   $da_members_form_fields_table = DA_MEMBERS_FORM_FIELDS_TABLE;
   $fields = $wpdb->get_results("SELECT label,field_name, priority, required FROM $da_members_form_fields_table");
-  //this array will store label=>field_name key pairs, will be used to extract the field_name by label key which user chooses during mapping
+  //this array will store label=>field_name key pairs from db table, will be used to extract the field_name by label key which user chooses during mapping
   $label_to_field_mapping_array = array();
   foreach ($fields as $field) {
     $label_to_field_mapping_array[trim_and_tolowercase($field->label)] = $field->field_name;
   }
+  //submit for mapping
   if (isset($_POST['submit_excel'])) {
     // Allowed mime types 
-    $excelMimes = array('text/xls', 'text/xlsx', 'application/excel', 'application/vnd.msexcel', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    $excel_mimes = array('text/xls', 'text/xlsx', 'application/excel', 'application/vnd.msexcel', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     // Validate whether selected file is a Excel file 
-    if (!empty($_FILES['excel-file']['name']) && in_array($_FILES['excel-file']['type'], $excelMimes)) {
+    if (!empty($_FILES['excel-file']['name']) && in_array($_FILES['excel-file']['type'], $excel_mimes)) {
       // If the file is uploaded 
       if (is_uploaded_file($_FILES['excel-file']['tmp_name'])) {
         $reader = new Xls();
@@ -69,20 +73,15 @@ function uploadFromExcel() {
         $worksheet = $spreadsheet->getActiveSheet();
         $worksheet_arr = array();
         $worksheet_arr = $worksheet->toArray();
-        $excelHeader = array_map('trim_and_tolowercase', $worksheet_arr[0]);
-        $number_of_excel_columns = count($excelHeader);
+        $excel_header = array_map('trim_and_tolowercase', $worksheet_arr[0]);
+        $number_of_excel_columns = count($excel_header);
         if ($number_of_excel_columns > 0) {
-          // //check if number of excel cols are greater than number of form table columns
-          // if ($number_of_excel_columns > count($fields)) {
-          //   $result['status'] = 'error';
-          //   $result['message'] = 'mismatch columns! number of columns should not be greater than ' . count($fields);
-          // } else {
-          // Remove header row and store excel data in session
+          //remove header row from excel data
           unset($worksheet_arr[0]);
           $_SESSION['excel-data'] = $worksheet_arr;
         }
       } else {
-        //if some how the file wan not found in temp folder
+        //if some how, the file wan not found in temp folder
         $result['status'] = 'error';
         $result['message'] = 'Some thing went wrong..';
       }
@@ -94,33 +93,59 @@ function uploadFromExcel() {
 
   //UPLOAD 
   if (isset($_POST['submit_excel_data'])) {
-    $assocArr = array();
+    $assoc_arr = array();
     //get the mapped columns from form
     $columns = $_POST['columns'];
-    $orignalFields = array();
+    $form_fields = array();
     foreach ($columns as $col) {
       //get the field name by key which user has chosen during mapping, save the field_name in array
-      if (trim_and_tolowercase($col) !== "ignore" && trim_and_tolowercase($col) !== "--select--") {
-        $orignalFields[] = $label_to_field_mapping_array[$col];
+      if (trim_and_tolowercase($col) !== "ignore" && trim_and_tolowercase($col) !== "-1") {
+        $form_fields[] = $label_to_field_mapping_array[$col];
+      } else {
+        $form_fields[] = $col;
       }
     }
+    // log_it($form_fields);
+
+    // check if user has not selected or ignored any required column
+    foreach ($fields as $field) {
+      if ($field->required == 1 && !in_array($field->field_name, $form_fields, true)) {
+        $result['status'] = 'error';
+        $result['message'] = 'Required columns not mapped!';
+      }
+    }
+
     //check if we have excel data in session
     if (isset($_SESSION['excel-data'])) {
       //get excel data from session
       $excel_rows = $_SESSION['excel-data'];
       //total rows of data in excel
-      $totalCount = count($excel_rows);
+      $total_count = count($excel_rows);
       foreach ($excel_rows as $excel_row) {
         //make an assoc array with key being field_name and value being excel row value
-        $assocRow = array_combine($orignalFields, $excel_row);
+        $assoc_row = array_combine($form_fields, $excel_row);
+        //check if user has not selected any field for mapping
+        if (array_key_exists('-1', $assoc_row)) {
+          $result['status'] = 'error';
+          $result['message'] = 'You have not selected some columns for mapping';
+        }
+        //remove any ignored column
+        if (array_key_exists('ignore', $assoc_row)) {
+          unset($assoc_row['ignore']);
+        }
         //store the assoc row one by one 
-        $assocArr[] = $assocRow;
+        $assoc_arr[] = $assoc_row;
       }
-      foreach ($assocArr as $record) {
+      // log_it($assoc_arr);
+      $error_row_indexes = array();
+      foreach ($assoc_arr as $index => $record) {
         //finally insert the data
-        $res = $wpdb->insert($da_members_table, $record);
-        if (!$res) {
-          $emptyRecords++;
+        if ($result['status'] !== 'error') {
+          $res = $wpdb->insert($da_members_table, $record);
+          if (!$res) {
+            $empty_records++;
+            array_push($error_row_indexes, $index);
+          }
         }
       }
     }
@@ -145,15 +170,15 @@ function uploadFromExcel() {
         <input type="submit" name="submit_excel" id="submit_excel" class="button button-primary" value="&nbsp;&nbsp;&nbsp;Map&nbsp;&nbsp;&nbsp;">
       </form>
       <?php
-      if (isset($number_of_excel_columns) /*&& !($number_of_excel_columns > count($fields))*/) { ?>
+      if (isset($number_of_excel_columns)  /*&& !($number_of_excel_columns > count($fields))*/) { ?>
         <form action="" method="post" style="text-align: center;">
           <div class="field-mapping-container" style="display: flex;flex-direction:column;gap:20px;margin-bottom:30px;margin-top:30px">
             <?php
-            foreach ($excelHeader as $col) { ?>
+            foreach ($excel_header as $col) { ?>
               <div class="mapping-row" style="display: flex;gap:20px;justify-content:space-between;align-items:center">
                 <span><?php echo $col; ?></span>
                 <select name="columns[]">
-                  <option value="--select--"> --select-- </option>
+                  <option value="-1"> --select-- </option>
                   <?php foreach ($fields as $field) {
                     $label = trim_and_tolowercase($field->label);
                     // $selected = $col  == $label ? 'selected' : '';
@@ -169,10 +194,13 @@ function uploadFromExcel() {
           <input type="submit" name="submit_excel_data" id="submit_exCel_data" class="button button-primary" value="&nbsp;&nbsp;&nbsp;Upload&nbsp;&nbsp;&nbsp;">
         </form>
       <?php }
-      if (isset($_POST['submit_excel_data'])) { ?>
+      if (isset($_POST['submit_excel_data']) && $result['status'] !== 'error') { ?>
         <div class="result-container">
-          <div class="inserted"><?php echo "inserted record : " . $totalCount - $emptyRecords;  ?></div>
-          <div class="not-inserted"><?php echo "records not inserted : " . $emptyRecords; ?></div>
+          <div class="inserted"><?php echo "inserted record : " . $total_count - $empty_records;  ?></div>
+          <div class="not-inserted">
+            <?php $error_message = count($error_row_indexes) > 1 ? "records with following row numbers have not been inserted : " :
+              "record with following row number has not been inserted : ";
+            echo $error_message . implode(" ", $error_row_indexes) . " , total : " . $empty_records; ?></div>
         </div>
       <?php }
       ?>
