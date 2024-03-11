1cc8b6512905eb10c8144ee34d18277839435372
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-03-11 16:01:46 +0530

    set checkbox checked of particular row if user changes its label

--- public/js/script.js
+++ public/js/script.js
@@ -4,34 +4,52 @@ jQuery(document).ready(function ($) {
   $('.delete_da_member').click(function () {
     $('.popup_container').fadeIn();
     let id = $(this).attr('data-del-id')
-    $('.popup_container .del_popup_actions #yes').attr('del_id', id)
-  })
-
-  $('.del_popup_actions #no').click(function () {
-    $('.popup_container').fadeOut();
+    $('.popup_container .del_popup_actions #yes').attr('del-id', id)
   })
   $('.del_popup_actions #yes').click(function () {
     // $('.popup_container').fadeOut();
-    let id = $(this).attr('del_id')
+    let id = $(this).attr('del-id')
     if (id) {
       location.replace(`http://localhost/wordpress/wp-admin/admin.php?page=da-members&del_id=${id}`)
     }
   })
+  $('.del_popup_actions #no').click(function () {
+    $('.popup_container').fadeOut();
+  })
+
+  $('#check_da_members_rows #cb-select-all-1').change(function () {
+    let row_check_boxes = $('.check_memb_rows');
+    let state = $(this).is(':checked') ? true : false
+    row_check_boxes.each((index, checkbox) => {
+      $(checkbox).prop('checked', state);
+    })
+  })
+
   //this will make checkbox checked when a form filed gains focus
+  preVal = {};
   let formFields = $('.form-field-input')
   formFields.each((index, field) => {
-    $(field).focus(function () {
+    //first get the previous text values from input fields, make object with key = index and value = text value
+    preVal = { ...preVal, [index]: ($(field).val()).toLowerCase() }
+    $(field).keyup(function () {
+      //check if value changed by comparing the previous value of input field with its current value,change state variable accordingly
+      let state = preVal[index] == ($(field).val()).toLowerCase() ? false : true;
       let id = $(field).attr('data-id');
       let checkbox = `#${id}`;
-      $(checkbox).prop('checked', true);
+      $(checkbox).prop('checked', state);
     })
   });
 
+  $('.req').change(function () {
+    console.log('====================================');
+    console.log('hello there im a checknbox' + " " + $(this).is(':checked'));
+    console.log('====================================');
+
+  })
+
+
   // pagination highlighting
   selectedPage = $('.pagination').attr('id')
-  console.log('====================================');
-  console.log(selectedPage);
-  console.log('====================================');
   let numberedLinks = $('.numbered_links > a')
   numberedLinks.eq(selectedPage - 1).addClass('active');
 
