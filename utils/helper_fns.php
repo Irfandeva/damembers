0752d4a05b19cf650ac1986abcefff2bca257b50
Irfandeva <logicparadise.irfandeva@gmail.com>
2024-04-01 12:13:07 +0530

    added a helper function to remove html tags from a string

--- utils/helper_fns.php
+++ utils/helper_fns.php
@@ -40,8 +40,15 @@ function sanitize_array($arr_item) {
   if (empty($arr_item)) return '';
   return sanitize_text_field($arr_item);
 }
-//remove html tags
+//remove html tags, receivs array
 function remove_html($array_item) {
   $array_item->bio = strip_tags(html_entity_decode(stripslashes($array_item->bio)));
+  // $array_item->bio = 'hello this is my bio';
+  return $array_item;
+}
+//remove html tags, receivs string
+function remove_html_from_string($array_item) {
+  $array_item = strip_tags(html_entity_decode(stripslashes($array_item)));
+  // $array_item->bio = 'hello this is my bio';
   return $array_item;
 }
